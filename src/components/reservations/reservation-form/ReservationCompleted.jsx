7ca6357bd90b916afb52e8f6ffbd1bdca3198d24
Nicolás David Faure <faureee89@gmail.com>
2023-05-01 09:23:26 -0300

    Styles of Reservation completed changed

--- src/components/reservations/reservation-form/ReservationCompleted.jsx
+++ src/components/reservations/reservation-form/ReservationCompleted.jsx
@@ -1,12 +1,53 @@
 import React from 'react'
 import '../../../assets/css/reservations/reservationCompleted.css'
 import checkIcon from '../../../assets/svg/check.svg'
+import { motion } from 'framer-motion'
+
 const ReservationCompleted = ({ reserva }) => {
   return (
-   <section className='Reservation__completed'>
-        <h4>Completaste con exito tu reserva {reserva[0].name} te esperamos :)</h4>
-        <button type='button'><img src={checkIcon} /></button>
-   </section>
+   <motion.section
+    initial={{
+      opacity:0.2
+    }}
+    animate={{
+      opacity:1,
+      transition:{
+        duration:1,
+        type:"spring"
+      
+      }}}
+    className='Reservation__completed'>
+        <motion.h4
+        initial={{
+          y:"-100px",
+          opacity:0
+          }}
+        animate={{
+          y:0,
+          opacity:1,
+          transition:{
+            duration:1,
+            type:"spring",
+            bounce:0.5
+          }
+        }}
+        >Completaste con exito tu reserva {reserva[0].name} te esperamos :)</motion.h4>
+        <motion.button
+          initial={{
+            y:"100px",
+            opacity:0
+            }}
+          animate={{
+            y:0,
+            opacity:1,
+            transition:{
+              duration:1,
+              type:"spring",
+              bounce:0.5
+            }
+          }}
+         type='button'>Confirmar<img src={checkIcon} /></motion.button>
+   </motion.section>
   )
 }
 
